38dc0b5e891864342ca9d3efbe6c1a49c3e0ab71
Haris Skiadas <cskiadas@gmail.com>
2024-08-25 22:11:41 -0400

    Add drag-and-drop, add-student-by-id, and removing students

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("DOMContentLoaded", () => {
   const fileSelector = document.getElementById("file-selector");
   fileSelector.addEventListener("change", (event) => {
@@ -35,6 +36,53 @@ window.addEventListener("DOMContentLoaded", () => {
     document.querySelector("main").classList.remove("small");
     document.querySelector("main").classList.add("large");
   });
+  // Manual add of users
+  document
+    .getElementById("manualAddSubmit")
+    .addEventListener("click", addStudentFromId);
+  document
+    .getElementById("manualAddToggle")
+    .addEventListener("click", () => { document.getElementById("manualAddSection").classList.toggle("hidden"); });
+  // Remove-user button
+  const studentsEl = document.getElementById("students");
+  studentsEl.addEventListener("click", (ev) => {
+    if (ev.target.matches("#students section .delete")) {
+      ev.target.closest("section").remove();
+    }
+  });
+  // Drag-and-drop
+  studentsEl.addEventListener("dragstart", (ev) => {
+    if (ev.target.matches("#students section *")) {
+      const node = ev.target.closest("#students section");
+      ev.dataTransfer.setData("text/plain", node.id);
+    }
+  });
+  studentsEl.addEventListener("dragenter", (ev) => {
+    if (ev.target.matches("#students section *")) {
+      ev.preventDefault();
+    }
+  });
+  studentsEl.addEventListener("dragover", (ev) => {
+    if (ev.target.matches("#students section *")) {
+      ev.preventDefault();
+    }
+  });
+  studentsEl.addEventListener("drop", (ev) => {
+    if (ev.target.matches("#students section *")) {
+      const target = ev.target.closest("section");
+      const sourceId = ev.dataTransfer.getData("text/plain");
+      if (sourceId != target.id) {
+        const source = document.getElementById(sourceId);
+        const studentsEl = document.getElementById("students");
+        const indexOf = [].indexOf.bind(studentsEl.children);
+        const sourcePosition = indexOf(source);
+        const targetPosition = indexOf(target);
+        const position = sourcePosition < targetPosition ? 'afterend' : 'beforebegin';
+        target.insertAdjacentElement(position, source);
+        ev.preventDefault();
+      }
+    }
+  });
 });
 
 function process(file) {
@@ -50,7 +98,11 @@ function processAsCSV(data) {
   const dataRows = csv.data.filter((arr) =>
     arr.some((s) => s.match(/@hanover.edu/))
   );
-  const results = dataRows.map(([name, _a, id, email]) => ({ name, email, id }));
+  const results = dataRows.map(([name, _a, id, email]) => ({
+    name,
+    email,
+    id,
+  }));
   createPageFrom(results);
 }
 
@@ -83,8 +135,20 @@ function createPageFrom(results) {
     const [last, firstNames] = name.split(/(?:\,\s+)/g);
     const [first, middle] = firstNames.split(/\s+/);
     const imgLinkOld = `https://my.hanover.edu/icsfileserver/icsphotos/${login}.jpg`;
-    const imgLink = `https://websites.hanover.edu/idphotos/${id}.jpg`
-    const html = `<section><img src="${imgLink}" /><h2 contenteditable="true">${first} ${last}</h2></section>`;
+    const fullName = `${first} ${last}`;
+    const html = makeStudentEntry({ id, name: fullName });
     studentsEl.insertAdjacentHTML("beforeend", html);
   }
+}
+
+function makeStudentEntry({ id, name }) {
+  const imgLink = `https://websites.hanover.edu/idphotos/${id}.jpg`;
+  return `<section id="student-${id}" draggable="true"><button class="delete">X</button><img src="${imgLink}" /><h2 contenteditable="true">${name}</h2></section>`;
+}
+
+function addStudentFromId() {
+  const id = document.getElementById("studentId").value;
+  const studentsEl = document.getElementById("students");
+  const html = makeStudentEntry({ id, name: "Type name here" });
+  studentsEl.insertAdjacentHTML("beforeend", html);
 }
